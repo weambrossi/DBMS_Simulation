f19f872c1690673ef125d22fb7d08a12b9cda2aa
Joud Bitar <bitjoud@gmail.com>
2026-03-27 11:43:51 -0400

    added all the handlers

--- mydb.c
+++ mydb.c
@@ -62,9 +62,6 @@ typedef struct {
    buffers.
 */
 
-int columnHeader[BLKMAX];
-int catalogHeader[BLKMAX];
-
 /* Depending on your implementation you might add or delete some of the arguments
    specified in the following functions */
 
@@ -108,7 +105,7 @@ void diskwrite(int blocknum, char *buffer) {
 
 // Purpose: initializes the specified header buffer and associates with the given header block.
 // Parameters: char *relname - name of the relation
-void dbcreate(char *relname) {
+int dbcreate(char *relname) {
 	int headerblknum;
 
 	// make sure relation does not already exist
@@ -120,13 +117,8 @@ void dbcreate(char *relname) {
 		headerblknum = findFreeBlock();
 		if (headerblknum == -1) {
 			printf("dbcreate error: no free disk block available\n");
-			return;
+			return -1;
 		}
-	}
-	// reserve a disk block for this relation's header
-	if (headerblknum == -1) {
-		printf("dbcreate error: no free disk block available\n");
-		return;
 	}
 
 	// add relation to relheaders table
@@ -141,6 +133,8 @@ void dbcreate(char *relname) {
 	diskwrite(headerblknum, (char *) relheaders[numrels].header);
 
 	numrels++;
+
+	return headerblknum;
 }
 	
 
@@ -197,23 +191,215 @@ void dbput(char *relname, char *tuple) {
 	diskwrite(blocknum, (char *)datablk);
 }
 
-	
-
-
-
-
 void do_create() {
 	char relname[9];
 	int n, k;
 	scanf("%s %d %d", relname, &n, &k);
-    // Read r n k from stdin
-	// Read n attribute specs (name + domain) from stdin
-	// Update the catalog (add a tuple for this new relation)
-	// Update the columns (add n tuples, one per attribute)
-	// Actually create the relation's storage structure via dbcreate()
+
+    // read n attribute specs
+    char attrnames[n][9];
+    char attrdomains[n];
+
+    for (int i = 0; i < n; i++) {
+        char domain[2];
+        scanf("%s %s", attrnames[i], domain);
+		if (domain[0] == 'I')
+			attrdomains[i] = 1;
+		else
+			attrdomains[i] = 0;
+    }
+
+	int relptr = dbcreate(relname);
+    char cattuple[RECSIZE];
+	sprintf(cattuple, "%s 0 %d %d 0 %d", relname, n, k, relptr);
+
+    // add to catalog: "relname kind attsize keysize relsize relptr"
+    sprintf(cattuple, "%s 0 %d %d 0 %d", relname, n, k, relptr);
+    dbput("catalog", cattuple);
+
+    // add to columns: one tuple per attribute
+    char coltuple[RECSIZE];
+    for (int i = 0; i < n; i++) {
+        sprintf(coltuple, "%s %s %d %d", relname, attrnames[i], attrdomains[i], i);
+        dbput("columns", coltuple);
+    }
 
 } 
 
+void do_delete() {
+    char relname[9];
+    scanf("%s", relname);
+    dbdelete(relname);
+}
+
+void do_insert() {
+    char relname[9];
+    int n;
+    scanf("%s %d", relname, &n);
+    char tuple[RECSIZE];
+	int c;
+    while ((c = getchar()) != '\n' && c != EOF);  // remove leftover newline
+
+    for (int i = 0; i < n; i++) {
+        fgets(tuple, RECSIZE, stdin);        
+		tuple[strcspn(tuple, "\n")] = '\0';  // strip trailing newline
+        // check key uniqueness
+        if (dbget(relname, tuple) != 0) {
+            printf("INSERT error: duplicate key in relation '%s'\n", relname);
+            continue;
+        }
+        dbput(relname, tuple);
+    }
+}
+
+void do_remove() {
+    char relname[9];
+    int n;
+    scanf("%s %d", relname, &n);
+    char tuple[RECSIZE];
+
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+    for (int i = 0; i < n; i++) {
+        fgets(tuple, RECSIZE, stdin);
+		tuple[strcspn(tuple, "\n")] = '\0';
+        if (dbget(relname, tuple) == 0) {
+            printf("REMOVE error: key not found in relation '%s'\n", relname);
+            continue;
+        }
+        dbremove(relname, slotnum);
+    }
+}
+
+void do_update() {
+    char relname[9];
+    int n;
+    scanf("%s %d", relname, &n);
+    char tuple[RECSIZE];
+	
+	int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+
+    for (int i = 0; i < n; i++) {
+        fgets(tuple, RECSIZE, stdin);
+		tuple[strcspn(tuple, "\n")] = '\0';
+        if (dbget(relname, tuple) == 0) {
+            printf("UPDATE error: key not found in relation '%s'\n", relname);
+            continue;
+        }
+        dbupdate(relname, slotnum, tuple);
+    }
+}
+
+void do_print() {
+    char relname[9];
+    scanf("%s", relname);
+    printf("%s\n", relname);
+    // print column headers from columns relation
+    // then print all tuples via dbread
+    char tuple[RECSIZE];
+    while (dbread(relname, tuple) != 0) {
+        printf("%s\n", tuple);
+    }
+}
+
+void do_union() {
+    char p[9], q[9], r[9];
+    scanf("%s %s %s", p, q, r);
+
+    // create new derived relation r
+    dbcreate(r);
+
+    // add all tuples from p
+    char tuple[RECSIZE];
+    while (dbread(p, tuple) != 0) {
+        dbwrite(r, tuple);
+    }
+
+    // add tuples from q only if not already in r
+    while (dbread(q, tuple) != 0) {
+        if (dbget(r, tuple) == 0)
+            dbwrite(r, tuple);
+    }
+
+    // update catalog and columns for r
+    // (copy column info from p since r has same attributes)
+}
+
+void do_difference() {
+    char p[9], q[9], r[9];
+    scanf("%s %s %s", p, q, r);
+
+    dbcreate(r);
+
+    char tuple[RECSIZE];
+    while (dbread(p, tuple) != 0) {
+        if (dbget(q, tuple) == 0)
+            dbwrite(r, tuple);
+    }
+}
+
+void do_projection() {
+    char p[9], q[9];
+    int n;
+    scanf("%s %s %d", p, q, &n);
+
+    char attrnames[n][9];
+    for (int i = 0; i < n; i++)
+        scanf("%s", attrnames[i]);
+
+    dbcreate(q);
+
+    char tuple[RECSIZE];
+    while (dbread(p, tuple) != 0) {
+        // extract only the specified attributes
+        char projected[RECSIZE];
+        // project tuple fields here based on attrnames
+        // then suppress duplicates
+        if (dbget(q, projected) == 0)
+            dbwrite(q, projected);
+    }
+}
+
+void do_selection() {
+    char p[9], q[9];
+    int n;
+    scanf("%s %s %d", p, q, &n);
+
+    char ops[n][3];
+    char vals[n][9];
+    for (int i = 0; i < n; i++)
+        scanf("%s %s", ops[i], vals[i]);
+
+    dbcreate(q);
+
+    char tuple[RECSIZE];
+    while (dbread(p, tuple) != 0) {
+        // check all n conditions against tuple
+        // if all pass, write to q
+        dbwrite(q, tuple);
+    }
+}
+
+void do_naturaljoin() {
+    char p[9], q[9], r[9];
+    int n;
+    scanf("%s %s %s %d", p, q, r, &n);
+
+    char attrnames[n][9];
+    for (int i = 0; i < n; i++)
+        scanf("%s", attrnames[i]);
+
+    dbcreate(r);
+
+    char ptuple[RECSIZE], qtuple[RECSIZE];
+    while (dbread(p, ptuple) != 0) {
+        while (dbread(q, qtuple) != 0) {
+            // check if common attributes match
+            // if yes, combine and write to r
+        }
+    }
+}
 
 int main(void) {
 
